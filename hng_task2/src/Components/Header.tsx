4e8d35885e2aa2bc841e8c2efeb34896249a2e0f
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-09-11 18:26:47 +0100

    Header UI in progress

---
+++ hng_task2/src/Components/Header.tsx
@@ -0,0 +1,26 @@
+import
+
+const Header = () => {
+  return (
+    <div className="bg-[url('/Poster.png')] bg-cover bg-center w-full h-screen text-white">
+        <div id="header" className="flex">
+            <div>
+                <img src=""/>
+                <p>MovieBox</p>
+            </div>
+            <div>
+                <input placeholder="What do you want to watch?"/>
+            </div>
+            <div>
+                <p>Sign In</p>
+                <img/>
+               
+
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Header
