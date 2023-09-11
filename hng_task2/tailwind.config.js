0f9c06dd02409ee5bfa597e67e4166f2317516b4
okorieonyedikachi <cosmasdaniella@gmail.com>
2023-09-11 18:26:31 +0100

    Icons

--- hng_task2/tailwind.config.js
+++ hng_task2/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        custom:["DM Sans", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
